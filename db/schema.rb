64dfabd58fa0108d1543c1eda0a08833420f4e24
Christopher Moody <moodyc21@gmail.com>
2018-02-26 10:04:03 -0500

    created seed file

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180224233847) do
+ActiveRecord::Schema.define(version: 20180226143435) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "boards", force: :cascade do |t|
+    t.string "name"
+    t.string "year"
+    t.boolean "completed"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_boards_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "provider", default: "email", null: false
@@ -44,4 +54,5 @@ ActiveRecord::Schema.define(version: 20180224233847) do
     t.index ["uid", "provider"], name: "index_users_on_uid_and_provider", unique: true
   end
 
+  add_foreign_key "boards", "users"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+ActiveRecord::Base.connection.tables.each do |t|
+    ActiveRecord::Base.connection.reset_pk_sequence!(t)
+  end
+
+
+User.destroy_all
+
+moody = User.create!(
+    email: 'moodyc21@gmail.com',
+    password: 'cm017914',
+    password_confirmation: 'cm017914'
+)
+
+sarah = User.create!(
+    email: 'sarahhampton@gmail.com',
+    password: 'blahblah',
+    password_confirmation: 'blahblah'
+)
+
+Board.create!(name: "moody's", year: "2012", completed: false, user: moody)
+
+Board.create!(name: "sarah's", year: "2016", completed: false, user: sarah)
